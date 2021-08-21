a9492ce4c1481e8c9fb3ca69ea8d77763347281b
luizlgs <63023028+luizlgs@users.noreply.github.com>
2021-08-21 17:47:10 -0300

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,17 +1,13 @@
+import threading
+
 path = "C:\\Users\\luigu\\PycharmProjects\\PythonProjects\\Chat_users"
 content = open(path).read().split('\n')
 all_items = [x.split(':') for x in content if x]
 database = dict(all_items)
+users_online = {}
 
 import socket
-import time
-print('Waiting some connection', end="")
-time.sleep(0.5)
-print('.', end="")
-time.sleep(0.5)
-print('.', end="")
-time.sleep(0.5)
-print('.')
+print('Waiting some connection')
 HOST = '127.0.0.1'
 PORT = 12999
 def server():
@@ -19,17 +15,35 @@ def server():
     server_socket.bind((HOST, PORT))
     server_socket.listen()
 
-    client_s, addr = server_socket.accept()
-    confirmation = client_s.recv(1024)
-    print('connected by', addr)
-    if confirmation.decode() == 'login':
-        login_confirmation(client_s)
-    if confirmation.decode() == 'register':
-        register(client_s)
+    def receive_clients():
+        while True:
+            try:
+                client_s, addr = server_socket.accept()
+                connection_alert = print('connected by', addr)
+                confirmation = client_s.recv(1024)
+            except:
+                continue
+            if confirmation.decode() == 'login':
+                login_confirmation(client_s)
+            if confirmation.decode() == 'register':
+                register(client_s)
+                try:
+                    if client_s.recv(1024).decode() == 's':
+                        client_s.sendall('true'.encode('utf-8'))
+                        login_confirmation(client_s)
+                    if client_s.recv(1024).decode() == 'n':
+                        break
+                except:
+                    break
+            clients_working = threading.Thread(receive_clients())
+    receive_clients()
 
 def login_confirmation(client_socket):
     while True:
-        user = client_socket.recv(1024).decode()
+        try:
+            user = client_socket.recv(1024).decode()
+        except:
+            break
         if user in database:
             client_socket.sendall('correct'.encode('utf-8'))
             confirmation = 'correct'
@@ -40,7 +54,10 @@ def login_confirmation(client_socket):
             continue
         if confirmation == 'correct':
             while True:
-                password = client_socket.recv(1024).decode()
+                try:
+                    password = client_socket.recv(1024).decode()
+                except:
+                    break
                 if password == database[user]:
                     confirmation = 'correct'
                     client_socket.sendall(confirmation.encode('utf-8'))
@@ -61,5 +78,6 @@ def register(server_socket):
     new_user = read_database.append(f'\n{str(name)}:{str(password)}')
     open_database_to_write = open('Chat_users', 'w')
     open_database_to_write.writelines(read_database)
-server()
+    database[name] = password
 
+server()
