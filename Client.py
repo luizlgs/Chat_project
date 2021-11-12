e9cf4626d2fec588ed4bb15c195e5e8390ca7648
luizlgs <63023028+luizlgs@users.noreply.github.com>
2021-11-11 22:53:27 -0300

    Update Client.py

--- Client.py
+++ Client.py
@@ -48,8 +48,19 @@ def menu():
 
         if option == 'c':
             s.sendall('inbox'.encode('utf-8'))
-            name = input('Digite o seu nome: ')
-            s.sendall(name.encode('utf-8'))
+            confirmation = s.recv(1024).decode()
+            if confirmation == 'give_me_the_name':
+                while True:
+                    name = input('Digite o seu nome: ')
+                    s.sendall(name.encode('utf-8'))
+                    confirmation = s.recv(1024).decode()
+                    if confirmation == 'valid_name':
+                        break
+                    if confirmation == 'invalid_name':
+                        print('Nome inválido')
+                        continue
+            if confirmation == 'ok':
+                pass
             inbox = s.recv(1024).decode()
             if len(inbox) > 1:
                 print('Suas mensagens:', inbox)
@@ -58,11 +69,19 @@ def menu():
 
         if option == 's':
             s.sendall('send'.encode('utf-8'))
-            friend_name = input('Nome da pessoa que deseja enviar a mensagem: ')
-            s.sendall(friend_name.encode('utf-8'))
-            message = input('Digite a sua mensagem: ')
-            s.sendall(message.encode('utf-8'))
-            print('\033[1;32mMensagem enviada!\033[m')
+            while True:
+                friend_name = input('Nome da pessoa que deseja enviar a mensagem: ')
+                s.sendall(friend_name.encode('utf-8'))
+                confirmation = s.recv(1024).decode()
+                if confirmation == 'valid_name':
+                    pass
+                if confirmation == 'invalid_name':
+                    print('Este usuário esta offline ou não existe')
+                    continue
+                message = input('Digite a sua mensagem: ')
+                s.sendall(message.encode('utf-8'))
+                print('\033[1;32mMensagem enviada!\033[m')
+                break
 
 while True:
     option = input('Login(l), register(r): ')
